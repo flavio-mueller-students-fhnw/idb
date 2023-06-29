31a9245c4431243456c8f7a7ef91fa13e1eb0be9
flavio-mueller-students-fhnw <flavio.mueller@students.fhnw.ch>
2023-06-30 01:40:42 +0200

    Added readme

--- Py/iot_hub.py
+++ Py/iot_hub.py
@@ -1,39 +1,82 @@
 from azure.iot.device import IoTHubDeviceClient, Message
+import time
+from grove.gpio import GPIO
+from grove.grove_temperature_humidity_sensor import DHT
+
+button = GPIO(5, GPIO.IN)
+dht11 = DHT("11", 16)
 
 # Connection string for your device
 CONNECTION_STRING = "HostName=flavio-idb-test-sponsor.azure-devices.net;DeviceId=idb-raspberry;SharedAccessKey=tyJbuaeUEalsgbLsH0kItK1MpjLbNf4FmJFneSqcYl0="
 
-# Define the message payload
-MESSAGE = "Hello, Azure IoT Hub!"
+client = None
+cache = {}
 
 
-def send_message():
+def create_client():
+    client = IoTHubDeviceClient.create_from_connection_string(
+        CONNECTION_STRING)
+    return client
+
+
+def connect_to_azure(client):
+    if client.connected:
+        print("Already connected to Azure IoT Hub.")
+    else:
+        try:
+            print("Connecting to Azure IoT Hub...")
+            client.connect()
+            print("Connected to Azure IoT Hub!")
+        except Exception as e:
+            print("Error connecting to Azure IoT Hub:", str(e))
+
+
+def disconnect_from_azure(client):
+    if client.connected:
+        try:
+            print("Disconnecting from Azure IoT Hub...")
+            client.disconnect()
+            print("Disconnected from Azure IoT Hub.")
+        except Exception as e:
+            print("Error disconnecting from Azure IoT Hub:", str(e))
+    else:
+        print("Already disconnected from Azure IoT Hub.")
+
+
+def send_message(client, message):
     try:
-        print("Connecting to Azure IoT Hub...")
-
-        # Create an IoT Hub client instance
-        client = IoTHubDeviceClient.create_from_connection_string(
-            CONNECTION_STRING)
-
-        # Connect to the IoT Hub
-        client.connect()
-        print("Connected to Azure IoT Hub!")
-
-        # Create a message object
-        message = Message(MESSAGE)
-
         print("Sending message to Azure IoT Hub...")
-        # Send the message to the IoT Hub
         client.send_message(message)
-        print("Message sent successfully!")
-
-        # Disconnect from the IoT Hub
-        client.disconnect()
-        print("Disconnected from Azure IoT Hub.")
-
+        print("Message sent to Azure IoT Hub.")
     except Exception as e:
-        print("Error sending message:", str(e))
+        print("Error sending message to Azure IoT Hub:", str(e))
+        cache[time.time()] = message
+
+    if len(cache) > 0 and client.connected:
+        try:
+            connect_to_azure(client)
+        except Exception as e:
+            print("Error connecting to Azure IoT Hub:", str(e))
+            return
+        for key in cache:
+            try:
+                print("Sending cached message to Azure IoT Hub...")
+                client.send_message(cache[key])
+                print("Cached message sent to Azure IoT Hub.")
+                del cache[key]
+            except Exception as e:
+                print("Error sending cached message to Azure IoT Hub:", str(e))
 
 
-# Call the function to send the message
-send_message()
+client = create_client()
+connect_to_azure(client)
+send_message(client, "Hello from Raspberry Pi!")
+
+while True:
+    time.sleep(5)
+    if button.read():
+        send_message(client, "Button pressed!")
+    else:
+        send_message(client, "Button released!")
+
+    send_message(client, "DHT:" + str(dht11.read))
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# idb
+# idb Architektur MiniChallenge Flavio Müller
+
+![Archtekturübersicht](Architecture/Architecture.png)
+
+## Beschreibung
+
+Als MiniChallenge wurden die beiden Geräte in die bestehende Cloud Architektur der VGT AG eingebunden.
+
+## Microcontroller
+
+Dem Microcontroller wurde das 4 Digit Display der Sonar Distanzsensor, sowie eine weiter LED angesteckt.
+Dabei zeigt der Display die aktuell gemessene Distanz in cm an.
+
+Zusätzlich läuft auf dem Microcontroller ein Webserver, welcher HTTP Calls entgegen nimmt und die onboard-LED entsprechend ansteuert.
+
+Die Kommunikation erfolgt über ein eigenes Teltonika Modem, welches mit einer SIM Karte ausgestattet ist.
+Diese Sim Karte ist Teil eins Virtual Networks in Azure von Quickline.
+
+Dieses VNet hat wiederum ein Peering zum VNet der VGT AG. Damit lassen sich über diverse Server (Device Manager, Core, etc.) Nachrichten an das Device schicken und so die LED steuern.
+
+## Raspberry Pi
+
+Der Raspberry PI ist mit einem Button und einem HDT11 Temperatursensor ausgestattet.
+Über das lokale Netzwerk sendet der Rapsberry Pi die Sensorwerte an einen Azure IOT Hub, welcher ebenfalls Teil des VGT VNet ist.
+
+Ausserdem wurde auf dem Raspberry Pi ein Cache hinzugefügt, welcher die Daten speichern würde, falls das Netzwerk nicht verfügbar ist.
+
+Bei einem Recovery des Netzwerks werden die Daten dann mit Timestamp an den IOT Hub gesendet.
